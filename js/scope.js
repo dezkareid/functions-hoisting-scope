326ab388156b40af3da4bf279dac9de32292b49c
DezkaReid <elmaildeldezkareid@gmail.com>
2017-10-25 02:57:19 -0500

    Ejemplo scope local

--- js/scope.js
+++ js/scope.js
@@ -2,6 +2,7 @@
 var mensaje = "Soy una variable global";
 
 function init() {
+	var mensaje = "Soy una variable local";
 	var body = document.querySelector('body');
 	var texto = document.createTextNode("El valor de la variable mensaje es : " + mensaje);
 	body.appendChild(texto);
